150502caa25482b3592bb040617bffd1ee8eaa3b
Hanna Mulugeta <hannabmulugeta@gmail.com>
2021-01-09 20:41:39 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -10,7 +10,7 @@ def apply_coupons(cart, coupons)
     coupons.each {|coupon_list|
       if grocery[:item] == coupon_list[:item] && grocery[:count] >= coupon_list[:num]
         grocery[:count] -= coupon_list[:num]
-        new_list[:item] = grocery[:item] + "W/COUPON"
+        new_list[:item] = grocery[:item] + " W/COUPON"
         new_list[:price] = coupon_list[:cost]/coupon_list[:num]
         new_list[:clearnce] = grocery[:clearance]
         new_list[:count] = coupon_list[:num]
